b40f9e1161e0e2e4c1827ca8c73d3ef467672cff
Ritik Kumar <thakurritik558@gmail.com>
2024-12-26 14:14:08 +0530

    added models

---
+++ timelessTastes-backend/src/main/java/com/ritik/timelessTastes_backend/model/User.java
@@ -0,0 +1,79 @@
+package com.ritik.timelessTastes_backend.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.ritik.timelessTastes_backend.dto.RestaurantDto;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String fullName;
+
+    private String email;
+
+    private String password;
+
+    private USER_ROLE role = USER_ROLE.ROLE_CUSTOMER;
+
+    @JsonIgnore
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "customer")
+    private List<Order> orders = new ArrayList<>();
+
+    @ElementCollection
+    private List<RestaurantDto> favourites = new ArrayList<>();
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Address> addresses =  new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public USER_ROLE getRole() {
+        return role;
+    }
+
+    public void setRole(USER_ROLE role) {
+        this.role = role;
+    }
+}
