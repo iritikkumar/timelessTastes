c2c25309491764f8c87c47f6926903dfd08b291b
Ritik Kumar <thakurritik558@gmail.com>
2024-12-26 15:42:59 +0530

    more models

---
+++ timelessTastes-backend/src/main/java/com/ritik/timelessTastes_backend/service/RestaurantService.java
@@ -0,0 +1,28 @@
+package com.ritik.timelessTastes_backend.service;
+
+import com.ritik.timelessTastes_backend.dto.RestaurantDto;
+import com.ritik.timelessTastes_backend.model.Restaurant;
+import com.ritik.timelessTastes_backend.model.User;
+import com.ritik.timelessTastes_backend.request.CreateRestaurantRequest;
+
+import java.util.List;
+
+public interface RestaurantService {
+    public Restaurant createRestaurant(CreateRestaurantRequest req, User user);
+
+    public Restaurant updateRestaurant(Long restaurantId, CreateRestaurantRequest updatedRestaurant) throws Exception;
+
+    public void deleteRestaurant(Long restaurantId) throws Exception;
+
+    public List<Restaurant> getAllRestaurant();
+
+    public List<Restaurant> searchRestaurant();
+
+    public Restaurant findRestaurantById(Long id) throws Exception;
+
+    public Restaurant getRestaurantByUserId(Long userId) throws Exception;
+
+    public RestaurantDto addToFavourites(Long restaurantId, User user) throws Exception;
+
+    public Restaurant updateRestaurantStatus(Long id) throws Exception;
+}
