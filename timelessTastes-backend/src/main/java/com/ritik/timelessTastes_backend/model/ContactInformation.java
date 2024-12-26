eb37c2258067b84de18fb3bc48f54754826cfcd4
Ritik Kumar <thakurritik558@gmail.com>
2024-12-26 14:26:39 +0530

    added more models

---
+++ timelessTastes-backend/src/main/java/com/ritik/timelessTastes_backend/model/ContactInformation.java
@@ -0,0 +1,11 @@
+package com.ritik.timelessTastes_backend.model;
+
+import lombok.Data;
+
+@Data
+public class ContactInformation {
+    private String email;
+    private String mobile;
+    private String twitter;
+    private String instagram;
+}
---
+++ timelessTastes-backend/src/main/java/com/ritik/timelessTastes_backend/model/USER_ROLE.java
@@ -0,0 +1,7 @@
+package com.ritik.timelessTastes_backend.model;
+
+public enum USER_ROLE {
+    ROLE_CUSTOMER,
+    ROLE_RESTAURANT_OWNER,
+    ROLE_ADMIN
+}
