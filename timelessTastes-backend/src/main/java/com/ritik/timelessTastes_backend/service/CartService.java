d915d61365a426a1be0261d68c9175fa3a63008e
Ritik Kumar <thakurritik558@gmail.com>
2025-01-03 22:32:45 +0530

    Cart service

---
+++ timelessTastes-backend/src/main/java/com/ritik/timelessTastes_backend/service/CartService.java
@@ -0,0 +1,23 @@
+package com.ritik.timelessTastes_backend.service;
+
+import com.ritik.timelessTastes_backend.model.Cart;
+import com.ritik.timelessTastes_backend.model.CartItem;
+import com.ritik.timelessTastes_backend.model.User;
+import com.ritik.timelessTastes_backend.request.AddCartItemRequest;
+
+public interface CartService {
+
+    public CartItem addItemToCart(AddCartItemRequest req, String jwt) throws Exception;
+
+    public CartItem updateCartItemQuantity(Long cartItemId, int quantity) throws Exception;
+
+    public Cart removeItemFromCart(Long cartItemId, String jwt) throws Exception;
+
+    public Long calculateCartTotals(Cart cart) throws Exception;
+
+    public Cart findCartById(Long id) throws Exception;
+
+    public Cart findCartByUserId(Long userId) throws Exception;
+
+    public Cart clearCart(Long userId) throws Exception;
+}
